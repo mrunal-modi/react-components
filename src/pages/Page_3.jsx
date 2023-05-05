b412c77a975c947014b1e707e6808204bf52a092
mrunal-modi <mrunal.modi@gmail.com>
2023-05-06 09:15:33 +1000

    test pages

---
+++ src/pages/Page_3.jsx
@@ -0,0 +1,19 @@
+
+const Page3 = {
+    "path": "/page3",
+    "navbarLinkLabel": "Page 3",
+    "order": 1,
+    "data": {},
+    "view": {
+        "section-text": {
+            "component": "Text",
+            "description": "Enim in fugiat reprehenderit incididunt est sunt tempor deserunt excepteur quis consequat nulla nostrud ex. In duis eiusmod do quis est in sit commodo consequat eu aliqua duis deserunt. Exercitation dolor mollit amet in sint do mollit magna. Lorem sunt sint aute incididunt aute. Ut consectetur non ipsum velit eu. Proident tempor esse voluptate consequat aliquip mollit. Aliquip labore consequat amet dolore minim reprehenderit amet tempor dolor incididunt nisi labore qui.",
+            "title": "Page 3",
+            "rtl": true,
+            // "bgColor": "#f2f2f2",
+            // "textColor": "#000",
+        }
+    }
+};
+
+export default Page3;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
 export Home from "./Home";
-export Home1 from "./Home_1";
-export Home2 from "./Home_2";
-export Home3 from "./Home_3";
+export Page1 from "./Page_1";
+export Page2 from "./Page_2";
+export Page3 from "./Page_3";
